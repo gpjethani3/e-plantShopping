dd541a55b23801a58cf81eb81fb36fb1b018c5b5
Girish <gpjethani@gmail.com>
2026-05-05 02:28:11 -0400

    Plant Cart Updated

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,7 +8,10 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
-    const CartItems = useSelector(state => state.cart.items);
+    const cartItems = useSelector(state => state.cart.items);
+    const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+  
+
     const dispatch = useDispatch()
 
     const plantsArray = [
@@ -282,7 +285,9 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>{totalItems > 0 && (
+          <span className="cart-total-badge">{totalItems}</span>
+        )}</h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -293,7 +298,7 @@ function ProductList({ onHomeClick }) {
                             <div>{category.category}</div>
                         </h1>
                         <div className="product-list">
-                            {category.plants.map((plant,plantIndex)=>
+                           {category.plants.map((plant,plantIndex)=>
                             <div className="product=card" key={plantIndex}>
                                 <img 
                                     className="product-image"
@@ -305,7 +310,7 @@ function ProductList({ onHomeClick }) {
                                 <div className="product-description">{plant.description}</div>
                                 <div className="product-cost">{plant.cost}</div>
                                 <button className="product-button" onClick={()=>handleAddToCart(plant)}>
-                                    Add to Card
+                                    Add to Cart
 
                                 </button>
 
